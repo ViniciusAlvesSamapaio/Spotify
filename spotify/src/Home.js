ac2b358a95c6eef62d06a8ee2d09477eac220f94
Emanuelle Neves <43800901+May199@users.noreply.github.com>
2021-03-17 12:02:05 -0300

    home em andamento

---
+++ spotify/src/Home.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <section className="Home-initial">
+        <div className="Home-menuBar"> 
+        
+        </div>
+
+        <h2>Vá de Premium. E seja feliz!</h2>
+        <a href="http://" className="Home-btn-initial">Iniciar o teste grátis</a>
+        <p className="text-p">* Sujeito a <a href="http://" className="text-sublinhado">Termos e condições</a>. Válido somente para usuários que ainda não experimentaram o Premium.</p>
+        
+      </section>
+
+    </div>
+  );
+}
+
+export default Home;
