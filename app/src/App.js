619564dc1a2e0c51c5d555d0a25f09d90cef5f93
Emanuelle Neves <43800901+May199@users.noreply.github.com>
2021-06-03 19:05:51 -0300

    remove

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,10 @@
-import React, {Component} from 'react';
 import './App.css';
+import React, {Component} from 'react';
 import Suporte from './views/Suporte';
 import Cadastro from './views/Cadastro';
 import Home from './views/Home';
 import Login  from './views/Login';
+
 import { 
   BrowserRouter as Router, 
   Switch, 
@@ -11,24 +12,7 @@ import {
 } from 'react-router-dom';
 
 class App extends Component { 
-  
-  // state = {
-  //   response: ''
-  // };
-  // componentDidMount(){
-  //   this.callApi()
-  //     .then(res => this.setState({response: res.express}))
-  //     .cath(error => console.log(error));
-  // }
-  // callApi = async () => {
-  //   const response = await fetch('/api');
-  //   const body = await response.json();
 
-  //   if(response.status !== 200) throw Error(body.message);
-
-  //   return body;
-  // };
-  
   render(){
     return(
       <Router>
