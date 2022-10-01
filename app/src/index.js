613ec30e372c791d87453b0fd021713b70396357
Emanuelle Neves <43800901+May199@users.noreply.github.com>
2022-10-01 12:54:23 -0300

    ADD Bootstrap

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.render(
   <React.StrictMode>
--- app/src/views/Login/index.js
+++ app/src/views/Login/index.js
@@ -22,19 +22,23 @@ export default class Login extends Component {
         return (
             <div>
                 <Menu />
-                <div className="login">
-                    <form onSubmit={this.handleSubmit}>
-                        <h3>Login</h3>
-                        <input type="email" placeholder="Email" className="info-input"
-                            onChange={e => this.email = e.target.value}/>
-                        <br/>
-                        <input type="password" placeholder="Password" className="info-input"
-                            onChange={e => this.password = e.target.value}/>
-                        <br/>
-                        <center>
-                            <button className="Home-btn-initial" type="submit">Login</button>
-                        </center>
-                    </form>
+                <div className="container">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email address" aria-describedby="emailHelp" style={{width: "25rem"}}/>
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style={{width: "25rem"}}/>
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
                 </div>
             </div>
         )
